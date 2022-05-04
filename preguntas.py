7f464a6a51acd441cbec98435edbb86a7c7d4811
yavelasquezo <yavelasquezo@unal.edu.co>
2022-05-03 20:09:26 -0500

    Update preguntas.py

--- preguntas.py
+++ preguntas.py
@@ -72,35 +72,34 @@ def pregunta_03():
     """
 
     # Lea el archivo `gm_2008_region.csv` y asignelo al DataFrame `df`
-    df = ____
+    df = pd.read_csv("gm_2008_region.csv", sep=",")
 
     # Asigne a la variable los valores de la columna `fertility`
-    X_fertility = ____
+    X_fertility = df['fertility'].to_numpy().reshape(-1, 1)
 
     # Asigne a la variable los valores de la columna `life`
-    y_life = ____
+    y_life = df['life'].to_numpy().reshape(-1, 1)
 
     # Importe LinearRegression
-    from ____ import ____
+    from sklearn.linear_model import LinearRegression
 
     # Cree una instancia del modelo de regresión lineal
-    reg = ____
+    reg = LinearRegression(fit_intercept=True,normalize=False,)
 
     # Cree El espacio de predicción. Esto es, use linspace para crear
     # un vector con valores entre el máximo y el mínimo de X_fertility
-    prediction_space = ____(
-        ____,
-        ____,
-    ).reshape(____, _____)
+    prediction_space = np.linspace(
+        X_fertility.min(),
+        X_fertility.max(),
+    ).reshape(-1, 1)
 
     # Entrene el modelo usando X_fertility y y_life
-    reg.fit(____, ____)
-
+    reg.fit(X_fertility, y_life)
     # Compute las predicciones para el espacio de predicción
     y_pred = reg.predict(prediction_space)
 
     # Imprima el R^2 del modelo con 4 decimales
-    print(____.score(____, ____).round(____))
+    print(reg.score(X_fertility, y_life).round(4))
 
 
 def pregunta_04():
@@ -110,38 +109,40 @@ def pregunta_04():
     """
 
     # Importe LinearRegression
+    from sklearn.linear_model import LinearRegression
     # Importe train_test_split
+    from sklearn.model_selection import train_test_split
     # Importe mean_squared_error
-    from ____ import ____
+    from sklearn.metrics import mean_squared_error, r2_score
 
     # Lea el archivo `gm_2008_region.csv` y asignelo al DataFrame `df`
-    df = ____
+    df = df = pd.read_csv("gm_2008_region.csv", sep=",")
 
     # Asigne a la variable los valores de la columna `fertility`
-    X_fertility = ____
+    X_fertility = df['fertility'].to_numpy().reshape(-1, 1)
 
     # Asigne a la variable los valores de la columna `life`
-    y_life = ____
+    y_life = df['life'].to_numpy().reshape(-1, 1)
 
     # Divida los datos de entrenamiento y prueba. La semilla del generador de números
     # aleatorios es 53. El tamaño de la muestra de entrenamiento es del 80%
-    (X_train, X_test, y_train, y_test,) = ____(
-        ____,
-        ____,
-        test_size=____,
-        random_state=____,
+    (X_train, X_test, y_train, y_test,) =  train_test_split(
+        X_fertility,
+        y_life,
+        test_size=0.2,
+        random_state=53,
     )
 
     # Cree una instancia del modelo de regresión lineal
-    linearRegression = ____
+    linearRegression = LinearRegression()
 
     # Entrene el clasificador usando X_train y y_train
-    ____.fit(____, ____)
+    linearRegression.fit(X_train, y_train)
 
     # Pronostique y_test usando X_test
-    y_pred = ____
+    y_pred = linearRegression.predict(X_test)
 
     # Compute and print R^2 and RMSE
     print("R^2: {:6.4f}".format(linearRegression.score(X_test, y_test)))
-    rmse = np.sqrt(____(____, ____))
+    rmse = np.sqrt(mean_squared_error(y_test, y_pred))
     print("Root Mean Squared Error: {:6.4f}".format(rmse))
